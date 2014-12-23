bbbeb704857658b6c1122634e36a72a3ddd7a369
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2014-12-23 15:44:46 -0600

    v2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,13 @@ module.exports = function(grunt) {
     compass: {
       dist: {
         options: {
-          config: 'config.rb'
+          sassDir: "app/sass",
+          cssDir: "app/css",
+          imagesDir: "app/images",
+          javascriptsDir: "app/scripts"
         }
       }
     },
-
-    // cssmin: {
-    // },
-
-    // uglify: {
-    // },
-
-    // concat: {
-    // },
 
     express: {
       server: {
@@ -25,15 +19,6 @@ module.exports = function(grunt) {
         }
       }
     },
-
-    open: {
-      all: {
-        path: 'http://localhost:3000'
-      }
-    },
-
-    // clean: {
-    // },
 
     watch: {
       css: {
@@ -47,24 +32,11 @@ module.exports = function(grunt) {
           livereload: true
         }
       }
-    },
-
-    imageoptim: {
-      dist: {
-        src: ['app/images']
-      }
     }
   });
 
-  // grunt.loadNpmTasks('grunt-contrib-clean');
-  // grunt.loadNpmTasks('grunt-contrib-concat');
-  // grunt.loadNpmTasks('grunt-contrib-cssmin');
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-imageoptim');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-express');
-  grunt.loadNpmTasks('grunt-open');
-
-  grunt.registerTask('default', ['compass', 'express', 'open', 'watch', 'express-keepalive']);
+  grunt.registerTask('default', ['compass', 'express', 'watch', 'express-keepalive']);
 };
