7755fe0a064f128218cf4ba4a84701592c6ab9d9
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2015-08-22 12:29:58 -0500

    WIP

--- app/scripts/journalController.js
+++ app/scripts/journalController.js
@@ -1,10 +1,60 @@
-angular.module('tweetsToSoftware')
-    .controller('journalController', function(switterServer, $scope, $http) {
-        'use strict';
+function Entry() {
+  this.interestingTweetsFound = '';
+  this.interestingTweetsDescription = '';
+  this.newThingsLeant = '';
+  this.newThingsDescription = '';
+  this.freeFormFeedback = '';
 
-        $scope.q1 = "";
-        $scope.q2 = "";
-        $scope.q3 = "";
-        $scope.q4 = "";
-        $scope.q5 = "";
-    });
+  this.errors = {};
+}
+
+Entry.prototype.isValid = function() {
+  this.errors = {};
+
+  if (this.interestingTweetsFound = '') {
+    this.errors.interestingTweetsFound = 'have to choose one';
+  }
+  if (this.interestingTweetsDescription = '') {
+    this.errors.interestingTweetsDescription = 'cannot be blank';
+  }
+  if (this.newThingsLeant = '') {
+    this.errors.newThingsLeant = 'have to choose one';
+  }
+  if (this.newThingsDescription = '') {
+    this.errors.newThingsDescription = 'cannot be blank';
+  }
+
+  return angular.equals(this.errors, {});
+};
+
+Entry.prototype.toJson = function() {
+  return {
+    interesting_tweets_found: this.interestingTweetsFound,
+    interesting_tweets_description: this.interestingTweetsDescription,
+    new_things_learnt: this.newThingsLeant,
+    new_things_description: this.newThingsDescription,
+    free_form_feedback: this.freeFormFeedback
+  }
+};
+
+angular.module('tweetsToSoftware')
+  .controller('journalController', function(switterServer, currentParticipant,
+                                            $scope, $http, $location) {
+    'use strict';
+
+    $scope.entry = new Entry();
+
+    $scope.submit = function() {
+      if ($scope.entry.isValid()) {
+        var data = $scope.entry.toJson();
+
+        data.participant_id = currentParticipant;
+
+        $http.post(switterServer + '/logger/journal', data)
+          .then(function() {
+            alert('thank you for you feedback!');
+            $location.path('/');
+          });
+      }
+    }
+  });
